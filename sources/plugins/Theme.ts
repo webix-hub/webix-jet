6ab1b5bce66389548a0e3e320a3550b72f60c250
Maksim Kozhukh <mkozhukh@ya.ru>
2017-10-05 14:15:50 +0300

    [update] regression in theme plugin

--- sources/plugins/Theme.ts
+++ sources/plugins/Theme.ts
@@ -3,12 +3,32 @@ import {IJetApp, IJetURL, IJetView} from "../interfaces";
 export function Theme(app: IJetApp, view: IJetView, config: any){
 	config = config || {};
 	const storage = config.storage;
-	let theme = storage ? storage.get("theme") : (config.theme || "flat");
+	let theme = storage ? storage.get("theme") : (config.theme || "flat-default");
 
 	const service = {
 		getTheme(){ return theme; },
 		setTheme(name:string, silent?:boolean){
-			// document.location.reload();
+			var parts = name.split("-");
+			var links = document.getElementsByTagName("link");
+
+			for (var i=0; i<links.length; i++){
+				var lname = links[i].getAttribute("title");
+				if (lname){
+					if (lname === name || lname === parts[0]){
+						links[i].rel = "stylesheet";
+						links[i].disabled = false;
+					} else {
+						links[i].rel = "alternate stylesheet";
+						links[i].disabled = true;
+					}
+					(webix as any).skin.set(parts[0]);
+				}
+				//remove old css
+				webix.html.removeCss(document.body, "theme-"+theme);
+				//add new css
+				webix.html.addCss(document.body, "theme-"+name);
+			}
+
 			theme = name;
 			if (storage){
 				storage.put("theme", name);
