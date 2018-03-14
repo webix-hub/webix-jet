b9c3aaa39e477276dc7d8cdd25402620730fec70
Maksim Kozhukh <mkozhukh@ya.ru>
2018-03-14 10:54:01 +0300

    [fix] back navigation with url router

--- dist/routers/UrlRouter.js
+++ dist/routers/UrlRouter.js
@@ -1,11 +1,15 @@
 var UrlRouter = (function () {
     function UrlRouter(cb, config) {
+        var _this = this;
         this.cb = cb;
+        window.onpopstate = function () { return _this.cb(_this.get()); };
         this.prefix = config.routerPrefix || "";
     }
     UrlRouter.prototype.set = function (path, config) {
         var _this = this;
-        window.history.pushState(null, null, this.prefix + path);
+        if (this.get() != path) {
+            window.history.pushState(null, null, this.prefix + path);
+        }
         if (!config || !config.silent) {
             setTimeout(function () { return _this.cb(path); }, 1);
         }
